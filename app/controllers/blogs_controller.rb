76a00d00b675df88a69dfc5333b8c501f4baa224
Hanada-Takao <taaa0823@gmail.com>
2022-02-03 22:34:33 +0900

    add_blog_destroy_&_refactoring

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+before_action :set_target_blog, only: %i[show edit update destroy]
+
   def index
     @blogs = Blog.all
   end
@@ -8,26 +10,33 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(blog_params)
+    blog = Blog.create(blog_params)
+    redirect_to blog
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    blog = Blog.find(params[:id])
-    blog.update(blog_params)
-    redirect_to blog
+    @blog.update(blog_params)
+    redirect_to @blog
+  end
+
+  def destroy
+    @blog.delete
+    redirect_to blogs_path
   end
 
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content, :image)
+  end
+
+  def set_target_blog
+    @blog = Blog.find(params[:id])
   end
 end
