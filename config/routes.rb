cc61ebfd6096366f8ac0b4118a3eebc92aded133
Hanada-Takao <taaa0823@gmail.com>
2022-02-03 18:23:55 +0900

    add_blog_create_&_index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'blogs', to: 'blogs#index'
   get 'blogs/new', to: 'blogs#new'
+  post 'blogs', to: 'blogs#create'
+  get 'blogs/:id', to: 'blogs#show'
 end
