64c242c8ebc57d2ab9e8c5c86dfc2e1d876c2afb
Hanada-Takao <taaa0823@gmail.com>
2022-02-03 16:56:06 +0900

    add_blog_view_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'blogs#index'
+  get 'blogs', to: 'blogs#index'
+  get 'blogs/new', to: 'blogs#new'
 end
