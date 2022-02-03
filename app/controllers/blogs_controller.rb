f97c606b77f148221e3eb481f42347e64afee37c
Hanada-Takao <taaa0823@gmail.com>
2022-02-03 23:11:02 +0900

    add_pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
 before_action :set_target_blog, only: %i[show edit update destroy]
 
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page])
   end
 
   def new
