3778b6cb4e5c827961a417084d44273cfd52e09b
Hanada-Takao <taaa0823@gmail.com>
2022-02-05 21:49:15 +0900

    add_comment_table

---
+++ app/models/comment.rb
@@ -0,0 +1,17 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :blog
+end
+
+# == Schema Information
+#
+# Table name: comments
+#
+#  id           :bigint           not null, primary key
+#  comment      :text             not null
+#  comment_name :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  blog_id      :bigint           not null
+#  user_id      :bigint           not null
+#
