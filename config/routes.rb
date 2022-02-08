bc9fd633da750a33ebedda98586fa8c3915e4702
Hanada-Takao <taaa0823@gmail.com>
2022-02-08 21:17:12 +0900

    Adjust_the_devise_area

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'home/top'
+  root to: "home#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show edit]
   resources :blogs do
     resources :comments, only: %i[create destroy]
   end
 
-  root 'blogs#index'
+  # root 'blogs#index'
 end
