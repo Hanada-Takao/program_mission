9552e227625478f3d9030251dca3f9d86316797f
Hanada-Takao <taaa0823@gmail.com>
2022-02-09 23:27:05 +0900

    add_relationships_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,17 @@ Rails.application.routes.draw do
   root to: "home#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+
   resources :blogs do
     resources :comments, only: %i[create destroy]
     resource :favorites, only: %i[create destroy]
   end
+
   # post '/favorites', to: 'favorites#create'
   # get 'favorites/index'
   # delete '/favorites', to: 'favorites#destroy'
