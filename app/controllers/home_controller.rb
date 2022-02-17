cfd4097aebf9c3285bedf02f52a3c4ccddc0471e
Hanada-Takao <taaa0823@gmail.com>
2022-02-17 09:26:38 +0900

    header_adjust

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,13 @@ class HomeController < ApplicationController
   end
 
   def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(email: 'guest_admin@example.com',
+                                  admin: 'true' ) do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now
-      user.user_name = "ゲスト"
+      user.user_name = "ゲストアドミン"
     end
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path, notice: 'ゲスト（管理者）ユーザーとしてログインしました。'
   end
 end
