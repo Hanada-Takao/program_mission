f7dc46e1f62438ee55c521cdc199f7a9fb101c96
Hanada-Takao <taaa0823@gmail.com>
2022-02-20 19:05:09 +0900

    add_conversation_models_spec

--- spec/factories/conversations.rb
+++ spec/factories/conversations.rb
@@ -9,8 +9,13 @@
 #  sender_id    :bigint           not null
 #
 FactoryBot.define do
-  factory :conversation do
-    sender_id { "" }
-    recipient_id { "" }
+  factory :conversation_1, class: Conversation do
+    sender_id { "2" }
+    recipient_id { "3" }
+  end
+
+  factory :conversation_2, class: Conversation do
+    sender_id { "3" }
+    recipient_id { "4" }
   end
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -14,22 +14,38 @@ RSpec.describe Conversation, type: :model do
 
   let!(:user_a) { create(:user_admin) }
   let!(:user_b) { create(:user_general) }
+  let!(:user_c) { create(:user_ordinary) }
+  let!(:conversation_a) { create(:conversation_2) }
 
-  context 'フォローの重複' do
+  context 'ユーザーidの重複' do
     it 'バリデーションに引っかかる' do
-      follow1 = create(:relationship_1)
-      follow1.save
-      follow2 = Relationship.new(follower_id: user_b.id, following_id: user_a.id)
-      expect(follow2).not_to be_valid
+      message_room_1 = create(:conversation_1)
+      message_room_1.save
+      message_room_2 = Conversation.new(sender_id: user_a.id, recipient_id: user_b.id)
+      expect(message_room_2).not_to be_valid
     end
   end
 
-  context 'フォローの重複' do
+  context 'ユーザーidの重複' do
     it 'バリデーションが通る' do
-      follow1 = create(:relationship_1)
-      follow1.save
-      follow2 = Relationship.new(follower_id: user_a.id, following_id: user_b.id)
-      expect(follow2).to be_valid
+      message_room_1 = create(:conversation_1)
+      message_room_1.save
+      message_room_2 = Conversation.new(sender_id: user_a.id, recipient_id: user_c.id)
+      expect(message_room_2).to be_valid
+    end
+  end
+
+  context "presence" do
+    it "バリデーションに引っかかる sender_idが無い場合" do
+      conversation_a.sender_id = nil
+      expect(conversation_a).to be_invalid
+    end
+  end
+  context "presence" do
+    it "バリデーションに引っかかる recipient_idが無い場合" do
+      conversation_a.recipient_id = nil
+      expect(conversation_a).to be_invalid
     end
   end
 end
+
