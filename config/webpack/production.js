b900545fe275a5e894296cda06bc0ed0a1371a63
Hanada-Takao <taaa0823@gmail.com>
2022-02-27 22:57:12 +0900

    Module not found: Error: Can't resolve './App' はresolveで直す_2

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,10 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = () => {
-    return {
-      resolve: {
-        extensions: ['.js', '.ts']
-      }
-    }
-  }
+module.exports = environment.toWebpackConfig()
